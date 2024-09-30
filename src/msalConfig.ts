8294602f4643ffa16bbc208f0875a25e61f82d43
Sayali Patil <sayalipatil@Sayalis-MacBook-Air.local>
2024-09-30 14:11:41 +0530

    Updated msalConfig file with clientID

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -2,8 +2,8 @@ import { Configuration } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: 'YOUR_CLIENT_ID', // Replace with your Azure AD client ID
-    authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // Replace with your Azure AD tenant ID
-    redirectUri: 'http://localhost:5173', // URL of your Vite dev server
+    clientId: 'e083fb20-233c-4902-b623-3c0687d5b2d8', // Replace with your Azure AD client ID
+    authority: 'https://login.microsoftonline.com/a6edc1af-dc14-4353-b633-622d9b4fcb9f', // Replace with your Azure AD tenant ID
+    redirectUri: 'http://localhost:3001', // URL of your Vite dev server
   },
 };
