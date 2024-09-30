aabee2cd57a99aee68ed3ad524a6fe580ba0e3fa
Sayali Patil <sayalipatil@Sayalis-MacBook-Air.local>
2024-09-30 15:24:31 +0530

    Remove sensitive info

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -2,8 +2,8 @@ import { Configuration } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: 'e083fb20-233c-4902-b623-3c0687d5b2d8', // Replace with your Azure AD client ID
-    authority: 'https://login.microsoftonline.com/a6edc1af-dc14-4353-b633-622d9b4fcb9f', // Replace with your Azure AD tenant ID
-    redirectUri: 'http://localhost:3001', // URL of your Vite dev server
+    clientId: import.meta.env.VITE_AZURE_CLIENT_ID, 
+    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`, 
+    redirectUri: 'http://localhost:3000', 
   },
 };
